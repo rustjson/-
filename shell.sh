3ee870bdfcad0876cb773376cb2e3d4896e8e2ef
Jason Young <4018764+rustjson@users.noreply.github.com>
2019-12-07 22:46:33 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
