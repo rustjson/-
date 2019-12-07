ae9c5e038699d0da130b19a205719276f4df4617
Jason Young <4018764+rustjson@users.noreply.github.com>
2019-12-07 22:44:34 +0800

    Create shell.sh

---
+++ shell.sh
@@ -0,0 +1,4 @@
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
