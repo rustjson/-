f1383ead36429304f3286be7341bf6683d832eeb
Jason <4018764+rustjson@users.noreply.github.com>
2022-05-17 22:48:38 +0200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -4,3 +4,9 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
+curl -O https://raw.githubusercontent.com/rustjson/-/master/.p10k.zsh
+curl -O https://raw.githubusercontent.com/rustjson/-/master/.zshrc
+# install font
+p10k configure
+
